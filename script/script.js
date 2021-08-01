2e3201ca9934f8d79e4fb5c940cfc6aed0b62f55
irunguliz <liz.irungu@student.moringaschool.com>
2021-08-01 23:26:21 +0300

    index.html

--- script/script.js
+++ script/script.js
@@ -1,1 +1,40 @@
- 
+//hovering effect 
+$(".image1").mouseover(function () {
+    $(".pizza1").show();
+    $(".image1").css("opacity", "0.5");
+});
+
+$(".image1").mouseout(function () {
+    $(".pizza1").hide();
+    $(".image1").css("opacity", "1");
+});
+
+$(".image2").mouseover(function () {
+    $(".pizza2").show();
+    $(".image2").css("opacity", "0.5");
+});
+
+$(".image2").mouseout(function () {
+    $(".pizza2").hide();
+    $(".image2").css("opacity", "1");
+});
+
+$(".image3").mouseover(function () {
+    $(".pizza3").show();
+    $(".image3").css("opacity", "0.5");
+});
+
+$(".image3").mouseout(function () {
+    $(".pizza3").hide();
+    $(".image3").css("opacity", "1");
+});
+
+$(".image4").mouseover(function () {
+    $(".pizza4").show();
+    $(".image4").css("opacity", "0.5");
+});
+
+$(".image4").mouseout(function () {
+    $(".pizza4").hide();
+    $(".image4").css("opacity", "1");
+});
